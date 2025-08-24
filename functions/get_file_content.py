fb760d1095879eef100680ba3fb6206f68016978
Trebino2 <maxtrebino@gmail.com>
2025-08-23 21:00:06 -0500

    get_file_content function and supporting files added

---
+++ functions/get_file_content.py
@@ -0,0 +1,20 @@
+import os
+from functions.config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+    full_abs_path = os.path.abspath(full_path)
+    abs_path_working = os.path.abspath(working_directory)
+    if not full_abs_path.startswith(abs_path_working):
+        print(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory.')
+    elif not os.path.isfile(full_abs_path):
+        print(f'Error: File not found or is not a regular file: "{file_path}"')
+    else:
+        try:
+            with open(full_abs_path, "r") as f:
+                file_content_string = f.read(MAX_CHARS)
+                if len(file_content_string) == 10000:
+                    file_content_string += "\n" + (f'[...File "{file_path}" truncated at 10000 characters]')
+                return file_content_string
+        except Exception as e:
+            print(f"Error: {e}")
